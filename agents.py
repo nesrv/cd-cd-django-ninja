aa583e5c93a60074cb79e0696728e835219ea93d
nesrv <ne_srv@mail.ru>
2026-02-27 00:47:32 +0300

    тест wrk

--- agents.py
+++ agents.py
@@ -1,8 +1,17 @@
 ab -n 10000 -c 1000 https://cd-cd-django-ninja-production.up.railway.app/api/products
-ab -n 10000 -c 1000 https://cd-cd-django-ninja-production.up.railway.app/products-django/
+ab -n 1000 -c 100 https://cd-cd-django-ninja-production.up.railway.app/products-django/
 
 ab -n 10000 -c 100 http://127.0.0.1:8000/api/products
 
+в 
+
+wrk -t4 -c200 -d30s http://127.0.0.1:8000/api/products
+wrk -t4 -c200 -d30s http://127.0.0.1:8000/products-django/
+wrk -t4 -c200 -d30s http://127.0.0.1:8000/api/health
+
+замени http://127.0.0.1:8000/
+
+https://cd-cd-django-ninja-production.up.railway.app
 
 http://127.0.0.1:8000
 
