ceca4d19b11ebcd2a7936bdba5399f6006b782df
nesrv <ne_srv@mail.ru>
2026-02-26 23:58:10 +0300

    workers 5 --threads 2 --timeout 30

---
+++ agents.py
@@ -0,0 +1,12 @@
+ab -n 10000 -c 1000 https://cd-cd-django-ninja-production.up.railway.app/api/products
+
+ab -n 10000 -c 100 http://127.0.0.1:8000/api/products
+
+
+http://127.0.0.1:8000
+
+uvicorn config.asgi:application --host 127.0.0.1 --port 8000
+
+
+pip install waitress
+waitress-serve --port=8000 config.wsgi:application
